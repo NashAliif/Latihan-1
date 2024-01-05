d8ccf28d3bb8819d8a49ed88c7025f75ee667688
Nashaliif <m.alief311@gmail.com>
2024-01-05 13:26:01 +0700

    conditional rendering

--- my-react-app/src/pages/register.jsx
+++ my-react-app/src/pages/register.jsx
@@ -1,17 +1,17 @@
 import FormRegister from "../components/Fragments/FormRegister";
 import AuthLayout from "../components/Layouts/AuthLayout";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 const RegisterPage = () => {
   return (
-    <AuthLayout title="Register">
+    <AuthLayout title="Register" type="register">
       <FormRegister />
-      <p className="text-sm mt-5 text-center">
+      {/* <p className="text-sm mt-5 text-center">
         Already have an account?{" "}
         <Link to="/login" className="font-bold text-blue-600">
           Login
         </Link>
-      </p>
+      </p> */}
     </AuthLayout>
   );
 };
