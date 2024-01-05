3f4966cd1e30882cf0f99833b8966f9d3cf9af8a
Nashaliif <m.alief311@gmail.com>
2024-01-05 16:13:00 +0700

    rendering list

--- my-react-app/src/pages/products.jsx
+++ my-react-app/src/pages/products.jsx
@@ -1,28 +1,41 @@
 import CardProduct from "../components/Fragments/CardProduct";
 
+const product = [
+  {
+    id: 1,
+    name: "Sepatu Baru",
+    price: "Rp 1.000.000",
+    image: "/images/shoes-1.jpg",
+    description: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima numquam harum totam, dolore dignissimos at sapiente, quasconsequuntur, facilis voluptate blanditiis tenetur sint ratione praesentium? Reiciendis quas officiis laborum ex!`,
+  },
+  {
+    id: 2,
+    name: "Sepatu Lama",
+    price: "Rp 500.000",
+    image: "/images/shoes-1.jpg",
+    description: `Lorem ipsum dolor sit amet consectetur, adipisicing elit.`,
+  },
+  {
+    id: 3,
+    name: "Sepatu Ada Dong",
+    price: "Rp 2.000.000",
+    image: "/images/shoes-1.jpg",
+    description: `Ini adalah sepatu baru`,
+  },
+];
+
 const ProductsPage = () => {
   return (
     <div className="flex justify-center py-5">
-      <CardProduct>
-        <CardProduct.Header image="/images/shoes-1.jpg" />
-        <CardProduct.Body title="Sepatu Baru">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima
-          numquam harum totam, dolore dignissimos at sapiente, quas
-          consequuntur, facilis voluptate blanditiis tenetur sint ratione
-          praesentium? Reiciendis quas officiis laborum ex!
-        </CardProduct.Body>
-        <CardProduct.Footer price="Rp 1.000.000" />
-      </CardProduct>
-      <CardProduct>
-        <CardProduct.Header image="/images/shoes-1.jpg" />
-        <CardProduct.Body title="Sepatu Baru">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima
-          numquam harum totam, dolore dignissimos at sapiente, quas
-          consequuntur, facilis voluptate blanditiis tenetur sint ratione
-          praesentium? Reiciendis quas officiis laborum ex!
-        </CardProduct.Body>
-        <CardProduct.Footer price="Rp 1.000.000" />
-      </CardProduct>
+      {product.map((product) => (
+        <CardProduct>
+          <CardProduct.Header image={product.image} />
+          <CardProduct.Body name={product.name}>
+            {product.description}
+          </CardProduct.Body>
+          <CardProduct.Footer price={product.price} />
+        </CardProduct>
+      ))}
     </div>
   );
 };
