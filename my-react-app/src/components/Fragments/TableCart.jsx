17ab7333067b810754cea6725aa9931e55354482
Nashaliif <m.alief311@gmail.com>
2024-01-08 20:09:19 +0700

    useReducer

--- my-react-app/src/components/Fragments/TableCart.jsx
+++ my-react-app/src/components/Fragments/TableCart.jsx
@@ -1,12 +1,18 @@
 import { useSelector } from "react-redux";
 import { useState, useEffect, useRef, useContext } from "react";
 import { DarkMode } from "../../context/DarkMode";
+import {
+  useTotalPrice,
+  useTotalPriceDispatch,
+} from "../../context/TotalPriceContext";
 
 const TableCart = (props) => {
   const { products } = props;
   const { isDarkMode } = useContext(DarkMode);
   const cart = useSelector((state) => state.cart.data);
-  const [totalPrice, setTotalPrice] = useState(0);
+  // const [totalPrice, setTotalPrice] = useState(0);
+  const dispatch = useTotalPriceDispatch();
+  const { total } = useTotalPrice();
 
   useEffect(() => {
     if (products.length > 0 && cart.length > 0) {
@@ -14,7 +20,13 @@ const TableCart = (props) => {
         const product = products.find((product) => product.id === item.id);
         return acc + product.price * item.qty;
       }, 0);
-      setTotalPrice(sum);
+      // setTotalPrice(sum);
+      dispatch({
+        type: "UPDATE",
+        payload: {
+          total: sum,
+        },
+      });
       localStorage.setItem("cart", JSON.stringify(cart));
     }
   }, [cart, products]);
@@ -77,7 +89,7 @@ const TableCart = (props) => {
             <b>
               {" "}
               ${" "}
-              {totalPrice.toLocaleString("id-ID", {
+              {total.toLocaleString("id-ID", {
                 styles: "currency",
                 currency: "USD",
               })}
--- my-react-app/src/components/Layouts/Navbar.jsx
+++ my-react-app/src/components/Layouts/Navbar.jsx
@@ -3,12 +3,14 @@ import { useLogin } from "../../hooks/useLogin";
 import Button from "../Elements/Button";
 import { useSelector } from "react-redux";
 import { DarkMode } from "../../context/DarkMode";
+import { useTotalPrice } from "../../context/TotalPriceContext";
 
 const Navbar = () => {
   const username = useLogin();
   const { isDarkMode, setIsDarkMode } = useContext(DarkMode);
   const [totalCart, setTotalCart] = useState(0);
   const cart = useSelector((state) => state.cart.data);
+  const { total } = useTotalPrice();
 
   useEffect(() => {
     const sum = cart.reduce((acc, item) => {
@@ -29,7 +31,7 @@ const Navbar = () => {
         Logout
       </Button>
       <div className="flex items-center bg-gray-800 p-2 rounded-md mx-5">
-        {totalCart}
+        Item : {totalCart} | Price : $ {total}
       </div>
       <Button
         className="right-2 top-2 bg-black text-white px-10 mx-5 rounded"
